6b5a50b3f2be022b331d3a0690e7b37f61c546f1
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2024-01-20 03:38:45 -0800

    client update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,28 @@
+import {Link} from "react-router-dom";
+import {CLIENT_URL} from "../utils/Utils";
+
 export const Dashboard = () => {
+    const arr = [
+        {name: 'Kategoriyalar', size: 1, link: '/category'},
+        {name: 'Mahsulotlar', size: 1, link: '/product'},
+        {name: 'Foydalanuvchilar', size: 1, link: '/users'},
+        {name: 'So\'rovlar', size: 1, link: '/request'},
+    ]
     return (
         <>
-            salom
+            <div className={"row col-12"}>
+                {arr.map(item => (
+                    <div className="col-3">
+                        <div className="card col-12">
+                            <h5 className="card-header">{item.name}</h5>
+                            <div className="card-body">
+                                <h6 className="card-title">{item.name}ning soni {item.size}ta</h6>
+                                <Link to={`${CLIENT_URL}${item.link}`} className="btn btn-primary" data-mdb-ripple-init>Batafsil</Link>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
