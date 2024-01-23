08e81bc5f3b40fd150e85db78b3ceb2eb8b75f45
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2024-01-23 02:39:38 -0800

    server url update

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,6 +1,6 @@
 export const CLIENT_URL = "/auth/online-fashion-shop/dashboard"
-export const SERVER_URL = "http://localhost/api/v1"
-// export const SERVER_URL = "https://online-fashion-bot-3a49d457053e.herokuapp.com/api/v1"
+// export const SERVER_URL = "http://localhost/api/v1"
+export const SERVER_URL = "https://online-fashion-bot-3a49d457053e.herokuapp.com/api/v1"
 
 export const SIDE_ARR = [
     {name: 'Asosiy bo\'lim', url: `${CLIENT_URL}`, logo: 'bi bi-house-check-fill'},
