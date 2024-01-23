5845be12a3135982d1158c4dc1e3943af86a950b
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2024-01-23 13:16:04 -0800

    clode

--- src/pages/user/Basket.jsx
+++ src/pages/user/Basket.jsx
@@ -33,6 +33,7 @@ export const Basket = () => {
             item.product[0].name + " " + item.size + " X " + (item.product[0].price - item.product[0].salePrice) + " = " + (item.size * (item.product[0].price - item.product[0].salePrice)) + "\n"
         ))}"\n\n\nUmumiy narxi = " + basket.allPrice&reply_markup={"inline_keyboard":%20[[{"text":%20"Tasdiqlash ✅",%20"callback_data":%20"sotib olaman : ${chatId}"}]]}`
         // window.location.href = "https://t.me/onlien_fashion_bot"
+        window.close();
     }
     return (
         <div>
@@ -114,11 +115,10 @@ export const Basket = () => {
                         <div className={"w-100 d-flex align-items-center justify-content-around p-3"}>
                             <button className={"btn btn-success w-50 m-1"}>{allPrice} so'm</button>
                             {path === "basket" ? (
-                                <a onClick={() => close()}
-                                   href={"https://t.me/onlien_fashion_bot"}
-                                   className={"btn btn-primary w-50 m-1"}>Buyurtma
+                                <button onClick={() => close()}
+                                        className={"btn btn-primary w-50 m-1"}>Buyurtma
                                     qilish
-                                </a>
+                                </button>
                             ) : (
                                 <Link to={`/basket/${chatId}`}
                                       className={"btn btn-primary w-50 m-1"}>Savatcha <span>{size}</span></Link>
