ddf5b66cab89b035ba2656acf3cd5f83b87d9951
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2024-01-23 13:12:24 -0800

    href

--- src/pages/user/Basket.jsx
+++ src/pages/user/Basket.jsx
@@ -32,7 +32,7 @@ export const Basket = () => {
         window.location.href = `https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${chatId}&text=${basket.productBaskets.map(item => (
             item.product[0].name + " " + item.size + " X " + (item.product[0].price - item.product[0].salePrice) + " = " + (item.size * (item.product[0].price - item.product[0].salePrice)) + "\n"
         ))}"\n\n\nUmumiy narxi = " + basket.allPrice&reply_markup={"inline_keyboard":%20[[{"text":%20"Tasdiqlash ✅",%20"callback_data":%20"sotib olaman : ${chatId}"}]]}`
-        window.location.href = "https://t.me/onlien_fashion_bot"
+        // window.location.href = "https://t.me/onlien_fashion_bot"
     }
     return (
         <div>
@@ -114,10 +114,11 @@ export const Basket = () => {
                         <div className={"w-100 d-flex align-items-center justify-content-around p-3"}>
                             <button className={"btn btn-success w-50 m-1"}>{allPrice} so'm</button>
                             {path === "basket" ? (
-                                <button onClick={() => close()}
-                                        className={"btn btn-primary w-50 m-1"}>Buyurtma
+                                <a onClick={() => close()}
+                                   href={"https://t.me/onlien_fashion_bot"}
+                                   className={"btn btn-primary w-50 m-1"}>Buyurtma
                                     qilish
-                                </button>
+                                </a>
                             ) : (
                                 <Link to={`/basket/${chatId}`}
                                       className={"btn btn-primary w-50 m-1"}>Savatcha <span>{size}</span></Link>
